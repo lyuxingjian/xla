0b1d0df0e44e61724806f5d8cf1bbf73b00b8808
lyuxingjian <63725682+lyuxingjian@users.noreply.github.com>
2021-04-10 10:05:09 +0800

    Update parallel_loader.py

--- torch_xla/distributed/parallel_loader.py
+++ torch_xla/distributed/parallel_loader.py
@@ -86,6 +86,7 @@ class ParallelLoader(object):
     self._devices = [torch.device(x) for x in devices]
     self._batchdim = batchdim
     self._batches_per_execution = batches_per_execution
+    self._per_device_samples = len(loader) // len(devices)
     self._done = False
     self._queues = dict()
     for device in self._devices:
@@ -114,7 +115,7 @@ class ParallelLoader(object):
     return PerDeviceLoader(self, torch.device(device))
 
   def per_device_samples(self):
-    return len(loader) // len(devices)
+    return self._per_device_samples
 
   def next_item(self, device):
     dqueue = self._queues[device]
